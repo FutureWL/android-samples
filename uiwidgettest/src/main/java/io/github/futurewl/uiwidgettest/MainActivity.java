400150c9da428f9a6f7e8d4159c3f6cb3c846c81
魏来 <624263934@qq.com>
2019-02-23 09:31:52 +0800

    第一行代码 Android 第二版|UI开发的点点滴滴 常用控件的用法

---
+++ uiwidgettest/src/main/java/io/github/futurewl/uiwidgettest/MainActivity.java
@@ -0,0 +1,45 @@
+package io.github.futurewl.uiwidgettest;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editText;
+
+    private ImageView imageView;
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        editText = findViewById(R.id.edit_text);
+        imageView = findViewById(R.id.image_view);
+        progressBar = findViewById(R.id.progress_bar);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("This is ProgressDialog");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
+                break;
+            default:
+                break;
+        }
+    }
+}
