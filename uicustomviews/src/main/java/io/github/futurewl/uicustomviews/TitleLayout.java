cd6539788bef21d3a5941de663294321e40d6c3f
魏来 <624263934@qq.com>
2019-02-25 23:19:55 +0800

    第一行代码 Android 第二版|UI开发的点点滴滴 系统控件不够用？创建自定义控件

---
+++ uicustomviews/src/main/java/io/github/futurewl/uicustomviews/TitleLayout.java
@@ -0,0 +1,38 @@
+package io.github.futurewl.uicustomviews;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2019/2/25:11:14 PM
+ * @version 1.0
+ */
+public class TitleLayout extends LinearLayout {
+
+    public TitleLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.title, this);
+        Button titleBack = findViewById(R.id.title_back);
+        Button titleEdit = findViewById(R.id.title_edit);
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity) getContext()).finish();
+            }
+        });
+        titleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "You clicked Edit button", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
