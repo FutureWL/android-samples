e70621dadd2843f787543c4f5ff8f114a6e05185
魏来 <624263934@qq.com>
2019-02-27 00:12:40 +0800

    第一行代码 Android 第二版|全局大喇叭 —— 详解广播 接收系统广播 静态注册实现开机启动

---
+++ broadcasttest/src/main/java/io/github/futurewl/broadcasttest/BootCompleteReceiver.java
@@ -0,0 +1,14 @@
+package io.github.futurewl.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Boot Complete 开机自启", Toast.LENGTH_SHORT).show();
+    }
+}
