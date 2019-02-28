bfa11cf19c9fef934b97c48d8fb4223ee8ea2c76
魏来 <624263934@qq.com>
2019-02-28 10:07:51 +0800

    第一行代码 Android 第二版|全局大喇叭 —— 详解广播 发送内部广播

--- broadcasttest/src/main/java/io/github/futurewl/broadcasttest/MainActivity.java
+++ broadcasttest/src/main/java/io/github/futurewl/broadcasttest/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -18,30 +19,57 @@ public class MainActivity extends AppCompatActivity {
 
     private IntentFilter intentFilter;
 
+    private LocalReceiver localReceiver;
+
+    private LocalBroadcastManager localBroadcastManager;
+
     private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intentFilter = new IntentFilter();
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver, intentFilter);
+//        使用本地广播
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("io.github.futurewl.broadcasttest.MY_BROADCAST");
-                sendOrderedBroadcast(intent, null);
+                Intent intent = new Intent("io.github.futurewl.broadcasttest.LOCAL_BROADCAST");
+                localBroadcastManager.sendBroadcast(intent);
             }
         });
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("io.github.futurewl.broadcasttest.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
+
+//        监听网络状态变化广播
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        networkChangeReceiver = new NetworkChangeReceiver();
+//        registerReceiver(networkChangeReceiver, intentFilter);
+
+//        发送静态自定义广播
+//        Button button = findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent("io.github.futurewl.broadcasttest.MY_BROADCAST");
+//                sendOrderedBroadcast(intent, null);
+//            }
+//        });
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(networkChangeReceiver);
+        if (networkChangeReceiver != null) {
+            unregisterReceiver(networkChangeReceiver);
+        }
+        if (localReceiver != null) {
+            unregisterReceiver(localReceiver);
+        }
     }
 
     class NetworkChangeReceiver extends BroadcastReceiver {
@@ -54,6 +82,13 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    class LocalReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "received local broadcast", Toast.LENGTH_SHORT).show();
         }
     }
 }
