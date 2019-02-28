a2b87da259ecb413ad7fe46ff1741425a0ce044d
魏来 <624263934@qq.com>
2019-02-28 11:05:39 +0800

    第一行代码 Android 第二版|数据存储全方案 —— 详解持久化技术 SharedPreferences存储

--- sharedpreferencestest/src/main/java/io/github/futurewl/sharedpreferencestest/MainActivity.java
+++ sharedpreferencestest/src/main/java/io/github/futurewl/sharedpreferencestest/MainActivity.java
@@ -1,13 +1,43 @@
 package io.github.futurewl.sharedpreferencestest;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button saveData = findViewById(R.id.save_data);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name", "Tom");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", false);
+                editor.apply();
+            }
+        });
+        Button restoreData = findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = getSharedPreferences("data", MODE_PRIVATE);
+                String name = preferences.getString("name", "");
+                int age = preferences.getInt("age", 0);
+                boolean married = preferences.getBoolean("married", false);
+                Log.d(TAG, "name is " + name);
+                Log.d(TAG, "age is " + age);
+                Log.d(TAG, "married is " + married);
+            }
+        });
     }
 }
